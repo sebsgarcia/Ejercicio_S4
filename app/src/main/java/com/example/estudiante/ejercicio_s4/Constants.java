12930b3449f4dd1fc5b02e38e57134580b9b5fb9
Sebastian Garcia <sebas971129@gmail.com>
2018-08-23 13:01:25 -0500

    Avance1

--- app/src/main/java/com/example/estudiante/ejercicio_s4/Constants.java
+++ app/src/main/java/com/example/estudiante/ejercicio_s4/Constants.java
@@ -3,4 +3,5 @@ package com.example.estudiante.ejercicio_s4;
 public class Constants {
 
     public static final int REQ_NEW_USER = 5000;
+    public static final int RESP_NEW_USER = 5001;
 }
--- app/src/main/java/com/example/estudiante/ejercicio_s4/UserFormActivity.java
+++ app/src/main/java/com/example/estudiante/ejercicio_s4/UserFormActivity.java
@@ -1,7 +1,10 @@
 package com.example.estudiante.ejercicio_s4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UserFormActivity extends AppCompatActivity {
@@ -9,6 +12,7 @@ public class UserFormActivity extends AppCompatActivity {
     private EditText edtUserFormName;
     private EditText edtUserFormEmail;
     private EditText edtUserFormUsername;
+    private Button btnUserFormFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,28 @@ public class UserFormActivity extends AppCompatActivity {
         edtUserFormName = findViewById(R.id.edt_userform_name);
         edtUserFormEmail = findViewById(R.id.edt_userform_email);
         edtUserFormUsername = findViewById(R.id.edt_userform_username );
+
+        btnUserFormFinish = findViewById(R.id.btn_userform_finish);
+
+        btnUserFormFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent respuesta = new Intent();
+
+                String name = edtUserFormName.getText().toString();
+                String email = edtUserFormEmail.getText().toString();
+                String username = edtUserFormUsername.getText().toString();
+
+                respuesta.putExtra( "name", name);
+                respuesta.putExtra( "email", email);
+                respuesta.putExtra( "username", username);
+
+                setResult(Constants.RESP_NEW_USER, respuesta);
+                finish();
+            }
+        });
+
+
 
     }
 }
